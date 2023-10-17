e4cf3c794bb8ca2f4092d2fd6b267bf230df8097
Nicolas Firpo <nicolas.firpo@davinci.edu.ar>
2023-10-17 12:09:02 -0300

    add styles

--- Clima/src/App.vue
+++ Clima/src/App.vue
@@ -1,7 +1,26 @@
 
 <template>
   <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Buscar.." v-model="query" v-on:keypress="fetchWeather"/> 
+        
 
+      </div>
+
+      <div class="weather-wrap">
+        <div class="location-box"> 
+         <div class="location">Buenos Aires, Ar</div>
+         <div class="date">Martes 17 octubre 2023</div> 
+        </div>
+         
+        
+      </div>
+      <div class="weather-box">
+        <div class="temp">9ºC</div>
+        <div class="weather">Lluvia</div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -11,8 +30,20 @@
     name: 'app',
     data (){
       return {
-        api_key: '';
+        api_key: 'a988a2448dd3fa0fd0275e95320fe80b',
+        url_base: 'https://api.openweathermap.org/data/2.5/',
+        query: '',
+        weather: {}
+
       }
+    },
+    methods: {
+      fetchWeather(e){
+        if(e.key == "enter"){
+          fetch(`${this.api_base}weater?q=${this.query}`)
+        }
+      }
+
     }
 
   }
@@ -20,10 +51,106 @@
 
 
 <style scoped>
-  #app {
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+
+body{
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+#app {
+    background-image: url(./assets/cold-bg.jpg);
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;  
   }
+
+main{
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0,0,0, 0.25), rgba(0,0,0, 0.75));
+
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+
+}
+
+.search-box .search-bar{
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+
+box-shadow: 0px 0px 16px rgba(0,0,0, 0,25);
+  background-color: rgba(255,255,255,0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0,0,0, 0.25);
+    background-color: rgba(255,255,255, 0.75);
+    border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location{
+  color: #fff;
+  font-size: 32px;
+  text-align: center;
+  font-weight: 500;
+  text-shadow: 1px 3px rgba(0,0,0,0.25);
+
+  
+}
+
+.location-box .date{
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box{
+  text-align: center;
+}
+
+.weather-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0,0,0, 0,25);
+  background-color: rgba(255,255,255,0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0,0,0, 0.25);
+
+}
+
+.weather-box .weather{
+  color: #fff;
+  font-size: 48px;
+  font-style: italic;
+  font-weight: 700;
+  text-shadow: 3px 6px rgba(0,0,0, 0,25);
+
+}
+
 </style>
