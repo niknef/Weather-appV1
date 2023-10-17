4cc1508688bc866e82f28ef2054689205fd877c9
Nicolas Firpo <nicolas.firpo@davinci.edu.ar>
2023-10-17 11:25:58 -0300

    first commit

---
+++ Clima/src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+
+<script>
+  export default{
+    name: 'app',
+    data (){
+      return {
+        api_key: '';
+      }
+    }
+
+  }
+</script>
+
+
+<style scoped>
+  #app {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
